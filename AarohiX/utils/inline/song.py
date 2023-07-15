15ae645fc0af16f74182b43932f160680e32b34e
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-07-15 09:26:52 +0530

    Update song.py

--- AarohiX/utils/inline/song.py
+++ AarohiX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="❣️sᴜᴩᴩᴏʀᴛ[ᴅᴀʙᴀɴɢ❣️]", url=f"{config.SUPPORT_GROUP}",
+                text="❣️sᴜᴩᴩᴏʀᴛ❣️", url=f"{config.SUPPORT_GROUP}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
