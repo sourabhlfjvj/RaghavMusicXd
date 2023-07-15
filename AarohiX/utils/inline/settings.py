87ff01766578f9263e4d242c2415eaa3269fe59e
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-07-15 09:27:53 +0530

    Update settings.py

--- AarohiX/utils/inline/settings.py
+++ AarohiX/utils/inline/settings.py
@@ -24,7 +24,7 @@ def setting_markup(_):
         ],
         [
             InlineKeyboardButton(
-                text="• sᴜᴩᴩᴏʀᴛ[❣️ᴅᴀʙᴀɴɢ❣️] •", url=f"{SUPPORT_GROUP}",
+                text="• ❣️sᴜᴩᴩᴏʀᴛ❣️ •", url=f"{SUPPORT_GROUP}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
